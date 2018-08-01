0d55439a13aca0e12fa6d6f6536d116950db5a98
william zhu <william_zhu@hotmail.ca>
2018-08-01 03:52:44 -0400

    fixed portfolio page and added node

--- assets/javascript/button.js
+++ assets/javascript/button.js
@@ -1,27 +1,30 @@
-$( document ).ready(function(){
+// $( document ).ready(function(){
 
-    $(document).on("click", ".hangman", function(){
-        window.open("https://github.com/william5656/word-guess-game")
-    });
 
-    $(document).on("click", ".collector", function(){
-        window.open("https://github.com/william5656/unit-4-game")
-    });
+//     $(document).on("click", ".hangman", function(){
+//         window.open("https://github.com/william5656/word-guess-game")
+//     });
 
-    $(document).on("click", ".triva", function(){
-        window.open("https://github.com/william5656/TriviaGame")
-    });
+//     $(document).on("click", ".collector", function(){
+//         window.open("https://github.com/william5656/unit-4-game")
+//     });
 
-    $(document).on("click", ".giphy", function(){
-        window.open("https://github.com/william5656/giphy-api")
-    });
+//     $(document).on("click", ".triva", function(){
+//         window.open("https://github.com/william5656/TriviaGame")
+//     });
 
-    $(document).on("click", ".RPS", function(){
-        window.open("https://github.com/william5656/RPS-Multiplayer")
-    });
+//     $(document).on("click", ".giphy", function(){
+//         window.open("https://github.com/william5656/giphy-api")
+//     });
 
-    $(document).on("click", ".superhero", function(){
-        window.open("https://github.com/dan-homoncik/group-project")
-    });
+//     $(document).on("click", ".RPS", function(){
+//         window.open("https://github.com/william5656/RPS-Multiplayer")
+//     });
 
-})
+//     $(document).on("click", ".superhero", function(){
+//         window.open("https://github.com/dan-homoncik/group-project")
+//     });
+
+
+
+// })
